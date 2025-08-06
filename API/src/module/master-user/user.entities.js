87be3d11c40bcce169a1cb93dfd47f846b1b3082
irhanmustofa <75462436+irhanmustofa@users.noreply.github.com>
2025-08-06 21:46:42 +0700

    update-register

--- API/src/module/master-user/user.entities.js
+++ API/src/module/master-user/user.entities.js
@@ -1,26 +1,42 @@
 import Validator from "../../database/database.validator.js";
-import { passwordHash } from "../../utils/functions.js";
+import { passwordHash, generateId } from "../../utils/functions.js";
 
 export default class User {
   constructor(input) {
     const baseRules = {
-      group_id: "required|string|min:3",
       name: "required|string|min:3|max:100",
       email: "required|email",
-      password: "password|min:8",
-      role: "required|number|min:0",
-      status: "required|number|min:0",
+      password: "required|string|min:8",
+      role: "numeric",
+      status: "numeric",
     };
 
     for (const [key, value] of Object.entries(input)) {
       this[key] = value;
     }
 
+    if (input.password) {
+      this.password = passwordHash(input.password);
+    }
+
+    if (this.subscription === undefined) {
+      this.subscription = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+    } else if (
+      typeof this.subscription === "string" ||
+      typeof this.subscription === "number"
+    ) {
+      this.subscription = new Date(this.subscription);
+    }
+
+    if (this.permission && !Array.isArray(this.permission)) {
+      this.permission = [this.permission];
+    }
+
     const validator = new Validator(input, baseRules);
     const result = validator.getResult();
 
-    if (input.password !== undefined)
-      this.password = passwordHash(input.password);
-    if (result.error.length > 0) this.errors = result.error;
+    if (result.error && result.error.length > 0) {
+      this.errors = result.error;
+    }
   }
 }
