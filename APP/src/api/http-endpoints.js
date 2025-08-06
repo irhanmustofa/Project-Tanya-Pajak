b1bb5f81397255404a5437f723f078df367f2c29
irhanmustofa <75462436+irhanmustofa@users.noreply.github.com>
2025-08-06 21:46:42 +0700

    update-register

--- APP/src/api/http-endpoints.js
+++
@@ -1,6 +0,0 @@
-const local_url = "http://localhost:5000";
-const server_url = "https://api-myacc.mytaxteknologi.site";
-
-const isDev = import.meta.env.MODE === "development";
-
-export const base_url = isDev ? local_url : server_url;
