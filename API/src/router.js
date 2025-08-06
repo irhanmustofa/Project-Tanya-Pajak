fd5a15dc96622dbb7b021991842846964c3445b2
irhanmustofa <75462436+irhanmustofa@users.noreply.github.com>
2025-08-06 21:46:42 +0700

    update-register

--- API/src/router.js
+++ API/src/router.js
@@ -7,15 +7,11 @@ import { validatingUser } from "./utils/middleware.js";
 import userRouter from "./module/master-user/user.route.js";
 import authRouter from "./module/auth/auth.route.js";
 import clientRouter from "./module/master-client/client.route.js";
-<<<<<<< HEAD
 import registerRouter from "./module/register/register.route.js";
 import userClientRouter from "./module/user-client/user.client.route.js";
-=======
->>>>>>> 979ceae (update files)
 
 const noValidate = ["/auth", "/register"];
 
-<<<<<<< HEAD
 // router.use(async (req, res, next) => {
 //   if (!noValidate.some((path) => req.path.startsWith(path))) {
 //     const is_valid = await validatingUser(req.headers);
@@ -24,18 +20,6 @@ const noValidate = ["/auth", "/register"];
 //       return Response(res, is_valid);
 //     }
 //   }
-=======
-router.use(async (req, res, next) => {
-  if (!noValidate.some((path) => req.path.startsWith(path))) {
-    // const is_valid = await validatingUser(req.headers);
-    // if (!is_valid.success) {
-    //   return Response(res, is_valid);
-    // }
-  }
-
-  next();
-});
->>>>>>> 979ceae (update files)
 
 //   next();
 // });
@@ -44,7 +28,6 @@ router.use("/register", registerRouter);
 //  AUTH
 router.use("/auth", authRouter);
 
-<<<<<<< HEAD
 // USERCLIENT
 router.use("/user-client", userClientRouter);
 
@@ -52,9 +35,4 @@ router.use("/user-client", userClientRouter);
 router.use("/client", clientRouter);
 router.use("/user", userRouter);
 
-=======
-//MASTER
-router.use("/client", clientRouter);
-router.use("/user", userRouter);
->>>>>>> 979ceae (update files)
 export default router;
