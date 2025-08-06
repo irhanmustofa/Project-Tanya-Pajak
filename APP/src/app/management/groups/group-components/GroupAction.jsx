a13d5cbfb5d96fc1245a6c585b10b48e9eb74545
irhanmustofa <75462436+irhanmustofa@users.noreply.github.com>
2025-08-06 21:46:42 +0700

    update-register

--- APP/src/app/management/groups/group-components/GroupAction.jsx
+++ APP/src/app/management/groups/group-components/GroupAction.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { MoreHorizontal, LucideEdit, LucideTrash } from "lucide-react";
@@ -10,7 +9,8 @@ import {
   DropdownMenuLabel,
   DropdownMenuContent,
 } from "@/components/ui/dropdown-menu";
-import { groupAll, groupsEndpoint } from "./GroupService";
+import { useState } from "react";
+import { groupAll, groupEndpoint } from "./GroupService";
 import { useDialog, useDialogDispatch } from "@/dialogs/DialogProvider";
 import { useGroup, useGroupDispatch } from "./GroupProvider";
 import GroupUpdateForm from "@/app/management/groups/group-pages/GroupUpdateForm";
@@ -19,8 +19,8 @@ import LoaderOverlay from "@/components/custom/loader-overlay";
 export default function GroupAction({ row }) {
   const dispatch = useDialogDispatch();
   const groupDispatch = useGroupDispatch();
-  const { dialogState, dialogAction, DialogDelete } = useDialog();
   const { groupAction } = useGroup();
+  const { dialogState, dialogAction, DialogDelete } = useDialog();
 
   const [onUpdate, setOnUpdate] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
@@ -34,7 +34,7 @@ export default function GroupAction({ row }) {
         message:
           "Are you sure you want to delete this group? This action cannot be undone.",
         status: "warning",
-        url: groupsEndpoint.delete(id),
+        url: groupEndpoint.delete(id),
       },
     });
   };
@@ -43,22 +43,18 @@ export default function GroupAction({ row }) {
     if (!success) return;
 
     setIsLoading(true);
-
     await groupAll().then((res) => {
       if (res.success) {
         groupDispatch({ type: groupAction.SUCCESS, payload: res.data });
       }
     });
-
     setIsLoading(false);
   };
 
   const item = row.original;
-
   return (
     <div className="relative">
       {isLoading && <LoaderOverlay />}
-
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="h-8 w-8 p-0">
@@ -73,17 +69,15 @@ export default function GroupAction({ row }) {
             <LucideEdit className="mr-2 h-4 w-4" />
             <Label>Update</Label>
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => handleDelete(item._id)}>
+          <DropdownMenuItem onClick={() => handleDelete(item.id)}>
             <LucideTrash className="mr-2 h-4 w-4" />
             <Label>Delete</Label>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
-
       {onUpdate && (
-        <GroupUpdateForm id={item._id} onClose={() => setOnUpdate(false)} />
+        <GroupUpdateForm id={item.id} onClose={() => setOnUpdate(false)} />
       )}
-
       {dialogState.isOpen && <DialogDelete onClose={handleOnCloseDelete} />}
     </div>
   );
