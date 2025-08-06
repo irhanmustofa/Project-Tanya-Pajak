2c7e5e532ca90a75c1c68b8ec3504c353f9d6411
irhanmustofa <75462436+irhanmustofa@users.noreply.github.com>
2025-08-06 21:46:42 +0700

    update-register

---
+++ APP/src/app/management/clients/client-components/ClientService.jsx
@@ -0,0 +1,141 @@
+import HttpRequest from "@/api/http-request";
+import { base_url } from "@/api/http-endpoints";
+
+export const clientEndpoint = {
+  all: `${base_url}/client`,
+  create: `${base_url}/client`,
+  import: `${base_url}/client/import`,
+  status: `${base_url}/client/status`,
+  deleteSome: `${base_url}/client/delete`,
+  get: (id) => `${base_url}/client/${id}`,
+  update: (id) => `${base_url}/client/${id}`,
+  delete: (id) => `${base_url}/client/${id}`,
+};
+
+export const clientAll = async () => {
+  try {
+    const request = await HttpRequest.method("GET")
+      .url(clientEndpoint.all)
+      .send();
+
+    return request;
+  } catch (error) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
+
+export const clientFirst = async (id) => {
+  try {
+    const request = await HttpRequest.method("GET")
+      .url(clientEndpoint.get(id))
+      .send();
+
+    return request;
+  } catch (error) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
+
+export const clientCreate = async (data) => {
+  try {
+    const request = await HttpRequest.method("POST")
+      .url(clientEndpoint.create)
+      .body(data)
+      .send();
+
+    return request;
+  } catch (error) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
+
+export const clientImport = async (data) => {
+  try {
+    const request = await HttpRequest.method("POST")
+      .url(clientEndpoint.import)
+      .body(data)
+      .send();
+
+    return request;
+  } catch (error) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
+
+export const clientUpdate = async (id, data) => {
+  try {
+    const request = await HttpRequest.method("PUT")
+      .url(clientEndpoint.update(id))
+      .body(data)
+      .send();
+
+    return request;
+  } catch (error) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
+
+export const clientObservationEndpoint = {
+  get: (id) => `${base_url}/client/observation/${id}`,
+  set: (id) => `${base_url}/client/observation/${id}`,
+};
+
+export const clientObservationGet = async (id) => {
+  try {
+    const request = await HttpRequest.method("GET")
+      .url(clientObservationEndpoint.get(id))
+      .send();
+
+    return request;
+  } catch (error) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
+
+export const clientObservationSet = async (id, data) => {
+  try {
+    const request = await HttpRequest.method("PUT")
+      .url(clientObservationEndpoint.set(id))
+      .body(data)
+      .send();
+
+    return request;
+  } catch (error) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
+
+export const ClientHistory = async (id) => {
+  try {
+    const request = await HttpRequest.method("GET")
+      .url(`${base_url}/client/history/${id}`)
+      .send();
+    return request;
+  } catch (error) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
