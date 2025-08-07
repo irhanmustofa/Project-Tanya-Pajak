0992d19b8d09031e0206eded37f62502a66b5ea1
irfangeliga <muhammad.2960@students.amikom.ac.id>
2025-08-07 20:35:35 +0700

    update files

--- API/src/module/master-client/controllers/update.client.js
+++ API/src/module/master-client/controllers/update.client.js
@@ -1,62 +1,21 @@
-import Response, { error } from "../../../app/response.js";
+import Response, { badRequest } from "../../../app/response.js";
 import MongodbWrapper from "../../../database/mongo/mongo.wrapper.js";
 import { moveFile } from "../../../utils/uploadHandler.js";
-import Client from "../master-client.entities.js";
+import MasterClient from "../master-client.entities.js";
 import { masterClientSchema } from "../master-client.schema.js";
 
-export default async function updateClient(req, res) {
+export default async function updateClient(req) {
   try {
     const wrapper = new MongodbWrapper(masterClientSchema());
+    const masterClient = new MasterClient(req.body);
 
-    const input = { ...req.body };
-
-    let legalitas_perusahaan = [];
-    if (req.body?.legalitas) {
-      try {
-        legalitas_perusahaan = typeof req.body.legalitas === 'string'
-          ? JSON.parse(req.body.legalitas)
-          : req.body.legalitas;
-      } catch (parseError) {
-        console.error("Error parsing legalitas_perusahaan:", parseError);
-        legalitas_perusahaan = [];
-      }
+    if (masterClient.errors) {
+      return badRequest({ message: masterClient.errors.join(", ") });
     }
 
-    input.legalitas_perusahaan = legalitas_perusahaan;
-    input.group_id = req.headers.groupid;
-    if (req.files?.logo) {
-      const newName = "logo-" + input.company_name;
-      const folder = "logo-client";
-
-      const fileResult = await moveFile(
-        req.files.logo,
-        newName,
-        folder,
-        ["image/jpeg", "image/png", "image/jpg", "image/gif", "image/webp"],
-        2
-      );
-
-      if (!fileResult.status) {
-        return Response(res, error({
-          message: fileResult.message || "Logo upload failed",
-        }));
-      } else {
-        input.logo = `/${fileResult.path}`;
-      }
-    }
-
-    const client = new Client(input);
-    if (client.errors) {
-      return Response(res, error({
-        message: client.errors,
-      }));
-    }
-
-    return Response(res, await wrapper.update(req.params.id, client));
+    return await wrapper.update(req.params.id, masterClient);
   } catch (error) {
-    console.error("Error updating client:", error);
-    return Response(res, error({
-      message: error.message || "An error occurred while updating the client.",
-    }));
+    console.log("Error updating client:", error);
+    return error({ message: "An error occurred while the system was running" });
   }
 }
