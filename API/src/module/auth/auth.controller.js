0c22a4a4ed5cdb9f473d02371692e776088b36a3
irfangeliga <muhammad.2960@students.amikom.ac.id>
2025-08-07 09:25:04 +0700

    update files

--- API/src/module/auth/auth.controller.js
+++ API/src/module/auth/auth.controller.js
@@ -3,11 +3,11 @@ import Response, {
   forbidden,
   success,
 } from "../../app/response.js";
-import { authConfig } from "../../config/config.js";
 import { createToken } from "../../utils/functions.js";
 import authRepositories from "./auth.repositories.js";
 import { authorizationSchema } from "./auth.schema.js";
 import authenticationProcess from "./controllers/authentication.process.js";
+import forgotPasswordController from "./controllers/forgot.password.js";
 import setLogin from "./controllers/set.login.js";
 
 const authorizationRepository = new authRepositories(authorizationSchema());
@@ -15,15 +15,17 @@ const authorizationRepository = new authRepositories(authorizationSchema());
 const login = async (req, res) => {
   const { email, password } = req.body;
   const { device } = req.headers;
-
   if (!email || !password || !device) {
     return Response(res, badRequest("some required fields are missing."));
   }
 
   const result = await setLogin({ email, password, device });
-
+  console.log(result);
   if (!result.success) {
-    return Response(res, result);
+    return Response(res, {
+      message: result.message || "Login failed.",
+      status: result.status || 500,
+    });
   }
 
   return Response(
@@ -64,14 +66,13 @@ const authentication = async (req, res) => {
     return Response(res, result);
   }
 
-  const { token, group_id, client_id } = result.data;
+  const { token, client_id } = result.data;
   return Response(
     res,
     success({
       message: "Authentication successful.",
       data: {
         token,
-        group_id,
         client_id,
       },
     })
@@ -86,11 +87,48 @@ const logout = async (req, res) => {
   );
 };
 
+const forgot = async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return Response(res, badRequest("Email is required."));
+  }
+
+  const result = await forgotPasswordController.forgotPassword(req, res);
+
+  if (!result.success) {
+    return Response(res, result);
+  }
+
+  return Response(
+    res,
+    success({
+      message:
+        "Reset Password Success! Please check your email to reset your password",
+    })
+  );
+};
+
+const resetPassword = async (req, res) => {
+  console.log("Resetting password with params:", req.params);
+  console.log("Request body:", req.body);
+
+  const result = await forgotPasswordController.resetPassword(req, res);
+
+  if (!result.success) {
+    return Response(res, result);
+  }
+
+  return Response(res, success({ message: "Password reset successful." }));
+};
+
 const AuthController = {
   login,
   logout,
   authentication,
   authorization,
+  forgot,
+  resetPassword,
 };
 
 export default AuthController;
