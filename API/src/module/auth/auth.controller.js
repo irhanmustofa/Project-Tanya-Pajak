d60fdd677bf8fc9f0b5d08bd1203115c11e71925
irfangeliga <muhammad.2960@students.amikom.ac.id>
2025-08-07 09:25:04 +0700

    update files

--- API/src/module/auth/auth.controller.js
+++ API/src/module/auth/auth.controller.js
@@ -7,6 +7,7 @@ import { createToken } from "../../utils/functions.js";
 import authRepositories from "./auth.repositories.js";
 import { authorizationSchema } from "./auth.schema.js";
 import authRegister from "./controllers/auth.register.js";
+import authVerification from "./controllers/auth.verification.js";
 import authenticationProcess from "./controllers/authentication.process.js";
 import forgotPasswordController from "./controllers/forgot.password.js";
 import setLogin from "./controllers/set.login.js";
@@ -20,7 +21,11 @@ const login = async (req, res) => {
     return Response(res, badRequest("some required fields are missing."));
   }
 
+<<<<<<< HEAD
   const result = await setLogin({ email, password, company_npwp, device });
+=======
+  const result = await setLogin({ email, password, device });
+>>>>>>> 0333528 (update files)
   if (!result.success) {
     return Response(res, {
       message: result.message || "Login failed.",
@@ -119,11 +124,16 @@ const resetPassword = async (req, res) => {
   return Response(res, success({ message: "Password reset successful." }));
 };
 
+const verification = async (req, res) => {
+  return Response(res, await authVerification(req));
+};
+
 const AuthController = {
   login,
   logout,
   register,
   authentication,
+  verification,
   authorization,
   forgot,
   resetPassword,
