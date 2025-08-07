cc2e2c2823e23baf3849c9bd9210416460d6cebb
irfangeliga <muhammad.2960@students.amikom.ac.id>
2025-08-07 09:41:39 +0700

    update files

--- API/src/module/coa/head/controllers/import.head.js
+++
@@ -1,6 +0,0 @@
-import MongodbWrapper from "../../../../database/mongo/mongo.wrapper.js";
-import { headSchema } from "../head.schema.js";
-
-export default async function importHead(req, res) {
-  return await wrapper.import(coaArr);
-}
