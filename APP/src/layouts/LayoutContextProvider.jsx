889d0acd87956a44d0e1813730942be38f117c80
irhanmustofa <75462436+irhanmustofa@users.noreply.github.com>
2025-08-08 17:51:10 +0700

    update-select-company

--- APP/src/layouts/LayoutContextProvider.jsx
+++ APP/src/layouts/LayoutContextProvider.jsx
@@ -8,6 +8,8 @@ export const LayoutProvider = ({ children }) => {
   const [company, setCompany] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [userPermissions, setUserPermissions] = useState([]);
+  const [role, setRole] = useState(null);
 
   useEffect(() => {
     const fetchCompany = async () => {
@@ -23,13 +25,17 @@ export const LayoutProvider = ({ children }) => {
         }
 
         const response = await userClientAll(clientId);
-
+        console.log("Company response:", response);
         if (response.success) {
           setCompany(response.data[0]);
+          setRole(response.data[0].role);
+          setUserPermissions(response.data[0].permission || []);
           setError(null);
         } else {
           setError(response.message || "Failed to fetch company");
           setCompany([]);
+          setRole(null);
+          setUserPermissions([]);
         }
       } catch (err) {
         console.error("LayoutProvider - Error:", err);
@@ -39,17 +45,30 @@ export const LayoutProvider = ({ children }) => {
         setLoading(false);
       }
     };
-
     fetchCompany();
   }, []);
+  console.log("User permissions:", userPermissions);
+
+  // Permission checker function
+  const hasPermission = (permissionKey) => {
+    if (!userPermissions || userPermissions.length === 0) {
+      return false;
+    }
+    return userPermissions.includes(permissionKey);
+  };
+
+  console.log("User permissions:", userPermissions);
 
   return (
     <layoutsContext.Provider
       value={{
         company,
+        role,
         setCompany,
+        userPermissions,
         loading,
         error,
+        hasPermission, // Function to check permissions
       }}
     >
       {children}
