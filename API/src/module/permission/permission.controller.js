f6285130d79ba29bddf110486f2b68ac977e01ea
irhanmustofa <75462436+irhanmustofa@users.noreply.github.com>
2025-08-11 18:43:13 +0700

    update-users-crud

--- API/src/module/permission/permission.controller.js
+++ API/src/module/permission/permission.controller.js
@@ -1,7 +1,4 @@
-import Response, {
-    badRequest,
-    success,
-} from "../../app/response.js";
+import Response, { badRequest, success } from "../../app/response.js";
 import MongodbWrapper from "../../database/mongo/mongo.wrapper.js";
 import Permission from "./permission.entities.js";
 import { permissionSchema } from "./permission.schema.js";
@@ -9,38 +6,39 @@ import { permissionSchema } from "./permission.schema.js";
 const wrapper = new MongodbWrapper(permissionSchema());
 
 const createPermission = async (req, res) => {
-    const permission = new Permission(req.body);
-    const result = await wrapper.create(permission);
+  const permission = new Permission(req.body);
+  const result = await wrapper.create(permission);
 
-    if (result.error) {
-        return Response(res, badRequest({ message: result.message }));
-    }
+  if (result.error) {
+    return Response(res, badRequest({ message: result.message }));
+  }
 
-    return Response(res, success({ message: "Permission created successfully" }));
+  return Response(res, success({ message: "Permission created successfully" }));
 };
 
 const getAllPermissions = async (req, res) => {
-    const permissions = await wrapper.allOrder("key", "asc");
-    if (permissions.error) {
-        return Response(res, badRequest({ message: permissions.message }));
-    }
-    const filteredData = permissions.data.map(permission => ({
-        _id: permission._id,
-        key: permission.key,
-        description: permission.description
-    }));
+  const permissions = await wrapper.allOrder("key", "asc");
+  if (permissions.error) {
+    return Response(res, badRequest({ message: permissions.message }));
+  }
+  const filteredData = permissions.data.map((permission) => ({
+    _id: permission._id,
+    key: permission.key,
+    description: permission.description,
+  }));
 
-    return Response(res, success({
-        message: "Permissions retrieved successfully",
-        data: filteredData
-    }));
+  return Response(
+    res,
+    success({
+      message: "Permissions retrieved successfully",
+      data: filteredData,
+    })
+  );
 };
 
-
-
 const PermissionController = {
-    createPermission,
-    getAllPermissions,
+  createPermission,
+  getAllPermissions,
 };
 
 export default PermissionController;
