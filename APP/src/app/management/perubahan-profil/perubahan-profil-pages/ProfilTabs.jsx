291b01fb5f4e9ccf29d9122dff42ff04326aefc0
irfangeliga <muhammad.2960@students.amikom.ac.id>
2025-08-11 21:44:17 +0700

    update files

--- APP/src/app/management/perubahan-profil/perubahan-profil-pages/ProfilTabs.jsx
+++ APP/src/app/management/perubahan-profil/perubahan-profil-pages/ProfilTabs.jsx
@@ -1,8 +1,5 @@
-import {
-  InputHorizontal,
-  InputVertical,
-} from "@/components/custom/input-custom";
-import ButtonSubmit from "@/components/custom/ButtonSubmit";
+import { InputVertical } from "@/components/custom/input-custom";
+import { Button } from "@/components/ui/button";
 import {
   Select,
   SelectTrigger,
@@ -12,74 +9,188 @@ import {
 } from "@/components/ui/select";
 import { useLocalStorage } from "@/hooks/use-local-storage";
 import { useValidateInput } from "@/hooks/use-validate-input";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useTransition } from "react";
 import {
   jenisWpOption,
   kppOption,
   badanHukumOption,
   statusNpwpOption,
+  kluOption,
 } from "@/helpers/variables";
-import { Button } from "react-day-picker";
+import { useDialog, useDialogDispatch } from "@/dialogs/DialogProvider";
+import {
+  useClient,
+  useClientDispatch,
+} from "../perubahan-profil-components/PerubahanProfilProvider";
+import {
+  clientFirst,
+  clientUpdate,
+} from "../perubahan-profil-components/PerubahanProfilService";
+import { dateShort, dateStrip } from "@/components/custom/DateFormatted";
 
-export default function ProfilTabs({ data }) {
-  const [input, setInput] = useState({});
+export default function ProfilTabs() {
   const id = useLocalStorage.get("clientId") ?? "";
+  const dialogDispatch = useDialogDispatch();
+  const clientDispatch = useClientDispatch();
+  const { dialogAction, dialogState, DialogInfo } = useDialog();
+  const { clientState, clientAction } = useClient();
+  const [input, setInput] = useState({});
+  const [isOpen, setIsOpen] = useState(false);
+  const [isPending, startTransition] = useTransition();
+  const getData = clientState.data[0] ?? {};
+
   const { errors, handleChange } = useValidateInput({
     schema: {
       company_name: "required|string|min:5",
+      company_npwp: "string|min:16",
+      kegiatan_utama: "string",
+      jenis_wp: "number",
+      bentuk_badan_hukum: "number",
+      status_npwp: "number",
+      tanggal_pengukuhan_pkp: "string",
+      status_pkp: "number",
+      kantor_wilayah_djp: "string",
+      kantor_pelayanan_pajak: "string",
+      seksi_pengawasan: "string",
+      kode_klu: "string",
     },
   });
 
   useEffect(() => {
-    setInput({
-      company_name: data.company_name,
-      npwp: data.npwp,
-      kegiatan_utama: data.kegiatan_utama,
-      jenis_wp: data.jenis_wp,
-      bentuk_badan_hukum: data.bentuk_badan_hukum,
-      status_npwp: data.status_npwp,
-      tanggal_pengukuhan_pkp: data.tanggal_pengukuhan_pkp,
-      status_pkp: data.status_pkp,
-      kantor_wilayah_djp: data.kantor_wilayah_djp,
-      kpp: data.kpp,
-      seksi_pengawasan: data.seksi_pengawasan,
+    if (clientState.success) {
+      setInput({
+        company_name: getData.company_name,
+        company_npwp: getData.company_npwp,
+        kegiatan_utama: getData.kegiatan_utama,
+        jenis_wp: getData.jenis_wp,
+        bentuk_badan_hukum: getData.bentuk_badan_hukum,
+        status_npwp: getData.status_npwp,
+        tanggal_pengukuhan_pkp: getData.tanggal_pengukuhan_pkp,
+        status_pkp: getData.status_pkp,
+        kantor_wilayah_djp: getData.kantor_wilayah_djp,
+        kantor_pelayanan_pajak: getData.kantor_pelayanan_pajak,
+        seksi_pengawasan: getData.seksi_pengawasan,
+        kode_klu: getData.kode_klu,
+      });
+    }
+    setIsOpen(true);
+  }, [getData, isOpen]);
+
+  const inputHandler = (event) => {
+    event.preventDefault();
+
+    startTransition(async () => {
+      const formData = new FormData();
+      formData.append("company_name", input.company_name);
+      formData.append("company_npwp", input.company_npwp);
+      formData.append("kegiatan_utama", input.kegiatan_utama);
+      formData.append("jenis_wp", input.jenis_wp);
+      formData.append("bentuk_badan_hukum", input.bentuk_badan_hukum);
+      formData.append("status_npwp", input.status_npwp);
+      formData.append("tanggal_pengukuhan_pkp", input.tanggal_pengukuhan_pkp);
+      formData.append("status_pkp", input.status_pkp);
+      formData.append("kantor_wilayah_djp", input.kantor_wilayah_djp);
+      formData.append("kantor_pelayanan_pajak", input.kantor_pelayanan_pajak);
+      formData.append("seksi_pengawasan", input.seksi_pengawasan);
+      formData.append("kode_klu", input.kode_klu);
+
+      await clientUpdate(id, formData).then((response) => {
+        if (!response.success) {
+          dialogDispatch({
+            type: dialogAction.DIALOG_INFO,
+            payload: {
+              isOpen: true,
+              title: "Update Profil Failed",
+              message: response.message,
+              status: "error",
+            },
+          });
+
+          handleCloseDialog();
+        } else {
+          dialogDispatch({
+            type: dialogAction.DIALOG_INFO,
+            payload: {
+              isOpen: true,
+              title: "Update Profile Success",
+              message: "Profile updated successfully",
+              status: "success",
+            },
+          });
+          clientFirst(id)
+            .then((res) => {
+              if (res.success) {
+                clientDispatch({
+                  type: clientAction.SUCCESS,
+                  payload: res.data,
+                });
+              }
+            })
+            .catch((err) => {
+              console.log("err:", err);
+            });
+
+          handleCloseDialog();
+        }
+      });
     });
-  }, [data]);
+  };
+
+  const handleCloseDialog = () => {
+    setIsOpen(false);
+    setInput({});
+  };
 
   return (
     <>
-      <form>
-        <div className="grid grid-cols-3 gap-4 my-2">
+      {dialogState.isOpen && <DialogInfo />}
+      <form onSubmit={inputHandler} className="px-6">
+        <div className="grid xl:grid-cols-3 grid-cols-1 md:grid-cols-2 gap-6 my-2">
           <InputVertical
-            title="Nama Perusahaan"
-            name="nama"
+            title={"Nama Perusahaan"}
+            name="company_name"
             onChange={(e) => {
               handleChange("company_name", e.target.value);
               setInput({ ...input, company_name: e.target.value });
             }}
-            value={data.company_name}
+            value={input.company_name}
             className="my-2"
           />
 
           <InputVertical
-            title="Nomor NPWP"
-            name="npwp"
-            onChange={(e) => handleChange("npwp", e.target.value)}
+            title={"Nomor NPWP" + "" + input.company_npwp}
+            name="company_npwp"
+            onChange={(e) => {
+              handleChange("company_npwp", e.target.value);
+              setInput({ ...input, company_npwp: e.target.value });
+            }}
             className="my-2"
+            value={input.company_npwp}
           />
 
           <InputVertical
             title="Kegiatan Utama"
             name="kegiatan_utama"
-            onChange={(e) => handleChange("kegiatan_utama", e.target.value)}
+            onChange={(e) => {
+              handleChange("kegiatan_utama", e.target.value);
+              setInput({ ...input, kegiatan_utama: e.target.value });
+            }}
             className="my-2"
+            value={input.kegiatan_utama}
           />
         </div>
 
-        <div className="grid grid-cols-4 gap-4 my-8">
+        <div className="grid xl:grid-cols-4 grid-cols-1 md:grid-cols-2 gap-6 my-8">
           <div>
             <h1 className="mb-2">Jenis Wajib Pajak</h1>
-            <Select name="jenis_wp" value={"" || undefined}>
+            <Select
+              name="jenis_wp"
+              value={String(input.jenis_wp)}
+              onValueChange={(e) => {
+                handleChange("jenis_wp", e);
+                setInput({ ...input, jenis_wp: e });
+              }}
+            >
               <SelectTrigger className="w-full">
                 <SelectValue placeholder="Pilih" />
               </SelectTrigger>
@@ -97,7 +208,14 @@ export default function ProfilTabs({ data }) {
 
           <div>
             <h1 className="mb-2">Bentuk Badan Hukum</h1>
-            <Select name="bentuk_badan_hukum" value={"" || undefined}>
+            <Select
+              name="bentuk_badan_hukum"
+              value={String(input.bentuk_badan_hukum)}
+              onValueChange={(e) => {
+                handleChange("bentuk_badan_hukum", e);
+                setInput({ ...input, bentuk_badan_hukum: e });
+              }}
+            >
               <SelectTrigger className="w-full">
                 <SelectValue placeholder="Pilih" />
               </SelectTrigger>
@@ -115,7 +233,14 @@ export default function ProfilTabs({ data }) {
 
           <div>
             <h1 className="mb-2">Status NPWP</h1>
-            <Select name="status_npwp" value={"" || undefined}>
+            <Select
+              name="status_npwp"
+              value={String(input.status_npwp)}
+              onValueChange={(e) => {
+                handleChange("status_npwp", e);
+                setInput({ ...input, status_npwp: e });
+              }}
+            >
               <SelectTrigger className="w-full">
                 <SelectValue placeholder="Pilih" />
               </SelectTrigger>
@@ -135,18 +260,26 @@ export default function ProfilTabs({ data }) {
               title="Tanggal Pengukuhan PKP"
               name="tanggal_pengukuhan_pkp"
               type="date"
-              value={data.tanggal_pengukuhan_pkp ?? ""}
-              onChange={(e) =>
-                handleChange("tanggal_pengukuhan_pkp", e.target.value)
-              }
+              value={dateStrip(input.tanggal_pengukuhan_pkp)}
+              onChange={(e) => {
+                handleChange("tanggal_pengukuhan_pkp", e.target.value);
+                setInput({ ...input, tanggal_pengukuhan_pkp: e.target.value });
+              }}
             />
           </div>
         </div>
 
-        <div className="grid grid-cols-4 gap-4 my-8">
+        <div className="grid xl:grid-cols-4 grid-cols-1 md:grid-cols-2 gap-6 my-8">
           <div>
             <h1 className="mb-2">Status PKP</h1>
-            <Select name="status_pkp" value={"" || undefined}>
+            <Select
+              name="status_pkp"
+              value={String(input.status_pkp)}
+              onValueChange={(e) => {
+                handleChange("status_pkp", e);
+                setInput({ ...input, status_pkp: e });
+              }}
+            >
               <SelectTrigger className="w-full">
                 <SelectValue placeholder="Pilih" />
               </SelectTrigger>
@@ -165,17 +298,25 @@ export default function ProfilTabs({ data }) {
             <InputVertical
               title="Kantor Wilayah DJP"
               name="kantor_wilayah_djp"
-              value={data.kantor_wilayah_djp ?? ""}
-              onChange={(e) =>
-                handleChange("kantor_wilayah_djp", e.target.value)
-              }
+              value={input.kantor_wilayah_djp}
+              onChange={(e) => {
+                handleChange("kantor_wilayah_djp", e.target.value);
+                setInput({ ...input, kantor_wilayah_djp: e.target.value });
+              }}
               className="mt-2 "
             />
           </div>
 
           <div>
-            <h1 className="mb-2">KPP</h1>
-            <Select name="kpp" value={"" || undefined}>
+            <h1 className="mb-2">Kantor Pelayanan Pajak</h1>
+            <Select
+              name="kantor_pelayanan_pajak"
+              value={String(input.kantor_pelayanan_pajak)}
+              onValueChange={(e) => {
+                handleChange("kantor_pelayanan_pajak", e);
+                setInput({ ...input, kantor_pelayanan_pajak: e });
+              }}
+            >
               <SelectTrigger className="w-full">
                 <SelectValue placeholder="Pilih" />
               </SelectTrigger>
@@ -193,7 +334,14 @@ export default function ProfilTabs({ data }) {
 
           <div>
             <h1 className="mb-2">Seksi Pengawasan</h1>
-            <Select name="seksi_pengawasan" value={"" || undefined}>
+            <Select
+              name="seksi_pengawasan"
+              value={String(input.seksi_pengawasan)}
+              onValueChange={(e) => {
+                handleChange("seksi_pengawasan", e);
+                setInput({ ...input, seksi_pengawasan: e });
+              }}
+            >
               <SelectTrigger className="w-full">
                 <SelectValue placeholder="Pilih" />
               </SelectTrigger>
@@ -208,9 +356,35 @@ export default function ProfilTabs({ data }) {
               </SelectContent>
             </Select>
           </div>
+          <div>
+            <h1 className="mb-2">Kode KLU</h1>
+            <Select
+              name="kode_klu"
+              value={String(input.kode_klu)}
+              onValueChange={(e) => {
+                handleChange("kode_klu", e);
+                setInput({ ...input, kode_klu: e });
+              }}
+            >
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Pilih" />
+              </SelectTrigger>
+              <SelectContent>
+                {kluOption.map((item, key) => {
+                  return (
+                    <SelectItem key={key} value={String(item.kode)}>
+                      {item.kode + "-" + item.deskripsi}
+                    </SelectItem>
+                  );
+                })}
+              </SelectContent>
+            </Select>
+          </div>
         </div>
         <div className="float-end">
-          <ButtonSubmit title="Submit" className="px-10 rounded-full" />
+          <Button pending={isPending} className="px-10 rounded-full">
+            Submit
+          </Button>
         </div>
       </form>
     </>
