51ba91e1adb806e250b87209218ab909ab8ad495
irfangeliga <muhammad.2960@students.amikom.ac.id>
2025-08-12 21:10:11 +0700

    update files

--- APP/src/app/management/perubahan-profil/perubahan-profil-pages/AlamatTabs.jsx
+++ APP/src/app/management/perubahan-profil/perubahan-profil-pages/AlamatTabs.jsx
@@ -4,11 +4,13 @@ import {
   useClientDispatch,
 } from "../perubahan-profil-components/PerubahanProfilProvider";
 import { useEffect, useState, useTransition } from "react";
-import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import { useLocalStorage } from "@/hooks/use-local-storage";
 import { useValidateInput } from "@/hooks/use-validate-input";
-import { InputVertical } from "@/components/custom/input-custom";
+import {
+  InputHorizontal,
+  InputVertical,
+} from "@/components/custom/input-custom";
 import {
   Select,
   SelectTrigger,
@@ -16,8 +18,12 @@ import {
   SelectItem,
   SelectValue,
 } from "@/components/ui/select";
-import { CheckSquare } from "lucide-react";
-import { jenisWpOption } from "@/helpers/variables";
+import { Checkbox } from "@/components/ui/checkbox";
+import { jenisAlamat, jenisWpOption } from "@/helpers/variables";
+import { Button } from "@/components/ui/button";
+import { countryList } from "../data/country";
+import { Input } from "@/components/ui/input";
+import { provinceList } from "../data/province";
 
 export default function AlamatTabs() {
   const id = useLocalStorage.get("clientId") ?? "";
@@ -26,6 +32,7 @@ export default function AlamatTabs() {
   const { dialogAction, dialogState, DialogInfo } = useDialog();
   const { clientState, clientAction } = useClient();
   const [input, setInput] = useState({});
+  const [country, setCountry] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const [isPending, startTransition] = useTransition();
   const getData = clientState.data[0] ?? {};
@@ -145,134 +152,219 @@ export default function AlamatTabs() {
     <>
       {dialogState.isOpen && <DialogInfo />}
       <form onSubmit={inputHandler} className="px-6">
-        <div className="grid xl:grid-cols-6 grid-cols-1 md:grid-cols-2 gap-6 my-2">
-          <div className="col-span-4">
-            {/* <Label>alamat</Label>
-            <Textarea name="alamat" /> */}
-            <InputVertical title="Alamat" name="alamat" />
-          </div>
-          <div className="col-span-1">
-            <InputVertical title="RT" name="rt" />
-          </div>
-          <div className="col-span-1">
-            <InputVertical title="RT" name="rt" type="number" />
-          </div>
-        </div>
-        <div className="grid xl:grid-cols-4 grid-cols-1 md:grid-cols-2 gap-6 my-2">
-          <div>
-            <h1 className="mb-2">Provinsi</h1>
-            <Select
-              name="provinsi"
-              onValueChange={(e) => {
-                handleChange("provinsi", e);
-                setInput({ ...input, provinsi: e });
-              }}
-            >
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder="Pilih" />
-              </SelectTrigger>
-              <SelectContent>
-                {jenisWpOption.map((item, key) => {
-                  return (
-                    <SelectItem key={key} value={String(item.kode)}>
-                      {item.jenis_wp}
-                    </SelectItem>
-                  );
-                })}
-              </SelectContent>
-            </Select>
-          </div>
-          <div>
-            <h1 className="mb-2">Kabupaten</h1>
-            <Select
-              name="kabupaten"
-              onValueChange={(e) => {
-                handleChange("kabupaten", e);
-                setInput({ ...input, kabupaten: e });
-              }}
-            >
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder="Pilih" />
-              </SelectTrigger>
-              <SelectContent>
-                {jenisWpOption.map((item, key) => {
-                  return (
-                    <SelectItem key={key} value={String(item.kode)}>
-                      {item.jenis_wp}
-                    </SelectItem>
-                  );
-                })}
-              </SelectContent>
-            </Select>
-          </div>
-          <div>
-            <h1 className="mb-2">Kecamatan</h1>
-            <Select
-              name="kecamatan"
-              onValueChange={(e) => {
-                handleChange("kecamatan", e);
-                setInput({ ...input, kecamatan: e });
-              }}
-            >
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder="Pilih" />
-              </SelectTrigger>
-              <SelectContent>
-                {jenisWpOption.map((item, key) => {
-                  return (
-                    <SelectItem key={key} value={String(item.kode)}>
-                      {item.jenis_wp}
-                    </SelectItem>
-                  );
-                })}
-              </SelectContent>
-            </Select>
-          </div>
-          <div>
-            <h1 className="mb-2">Desa</h1>
-            <Select
-              name="desa"
-              onValueChange={(e) => {
-                handleChange("desa", e);
-                setInput({ ...input, desa: e });
-              }}
-            >
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder="Pilih" />
-              </SelectTrigger>
-              <SelectContent>
-                {jenisWpOption.map((item, key) => {
-                  return (
-                    <SelectItem key={key} value={String(item.kode)}>
-                      {item.jenis_wp}
-                    </SelectItem>
-                  );
-                })}
-              </SelectContent>
-            </Select>
-          </div>
-          <div>
-            <InputVertical
-              name="kode_area"
-              title="Kode Wilayah"
-              onChange={(e) => {
-                handleChange("kode_area", e.target.value);
-                setInput({ ...input, kode_area: e.target.value });
-              }}
-            />
-          </div>
-          <div>
-            <InputVertical
-              name="kode_pos"
-              title="Kode Pos"
-              onChange={(e) => {
-                handleChange("kode_pos", e.target.value);
-                setInput({ ...input, kode_pos: e.target.value });
-              }}
-            />
-          </div>
-          <div>
-            <InputVertical
+        {/* <div className="grid xl:grid-cols-6 grid-cols-1 md:grid-cols-2 gap-6 my-4  items-end"> */}
+        <div className="grid xl:grid-cols-3 grid-cols-1 gap-6 my-4  items-end">
+          <div className="col-span-2">
+            <div className="grid grid-cols-4 my-2">
+              <div className="col-span-1">
+                <h1 className="font-medium mb-2">Negara</h1>
+              </div>
+              <div className="col-span-3">
+                <Select
+                  name="Negara"
+                  onValueChange={(e) => {
+                    handleChange("negara", e);
+                    setInput({ ...input, negara: e });
+                    setCountry(e);
+                  }}
+                >
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Pilih" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {countryList.map((item, key) => {
+                      return (
+                        <SelectItem key={key} value={String(item.kode)}>
+                          {item.name}
+                        </SelectItem>
+                      );
+                    })}
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-4 my-2">
+              <div className="col-span-1">
+                <h1 className="font-medium mb-2">Jenis Alamat</h1>
+              </div>
+              <div className="col-span-3">
+                <Select
+                  name="jenis_alamat"
+                  onValueChange={(e) => {
+                    handleChange("jenis_alamat", e);
+                    setInput({ ...input, jenis_alamat: e });
+                    setCountry(e);
+                  }}
+                >
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Pilih" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {jenisAlamat.map((item, key) => {
+                      return (
+                        <SelectItem key={key} value={String(item.kode)}>
+                          {item.jenis}
+                        </SelectItem>
+                      );
+                    })}
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+
+            <InputHorizontal title="Alamat" name="alamat" />
+
+            <div className="grid grid-cols-4 my-2">
+              <div className="col-span-1">
+                <h1>RT/RW</h1>
+              </div>
+              <div className="xl:col-span-1 col-span-3 flex">
+                <Input name="rt" />
+                <h1 className="text-center mx-4 text-[20px]">/</h1>
+                <Input name="rt" />
+              </div>
+            </div>
+
+            <div className="grid grid-cols-4 my-2">
+              <div className="col-span-1">
+                <h1 className="mb-2">Provinsi</h1>
+              </div>
+              <div className="col-span-3">
+                <Select
+                  name="provinsi"
+                  onValueChange={(e) => {
+                    handleChange("provinsi", e);
+                    setInput({ ...input, provinsi: e });
+                  }}
+                >
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Pilih" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {provinceList.map((item, key) => {
+                      return (
+                        <SelectItem key={key} value={String(item.kode)}>
+                          {item.name}
+                        </SelectItem>
+                      );
+                    })}
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-4 my-2">
+              <div className="col-span-1">
+                <h1 className="mb-2">Kabupaten</h1>
+              </div>
+              <div className="col-span-3">
+                <Select
+                  name="kabupaten"
+                  onValueChange={(e) => {
+                    handleChange("kabupaten", e);
+                    setInput({ ...input, kabupaten: e });
+                  }}
+                >
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Pilih" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {provinceList.map((item, key) => {
+                      return (
+                        <SelectItem key={key} value={String(item.kode)}>
+                          {item.name}
+                        </SelectItem>
+                      );
+                    })}
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-4 my-2">
+              <div className="col-span-1">
+                <h1 className="mb-2">Kecamatan</h1>
+              </div>
+              <div className="col-span-3">
+                <Select
+                  name="kecamatan"
+                  onValueChange={(e) => {
+                    handleChange("kecamatan", e);
+                    setInput({ ...input, kecamatan: e });
+                  }}
+                >
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Pilih" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {jenisWpOption.map((item, key) => {
+                      return (
+                        <SelectItem key={key} value={String(item.kode)}>
+                          {item.jenis_wp}
+                        </SelectItem>
+                      );
+                    })}
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-4 my-2">
+              <div className="col-span-1">
+                <h1 className="mb-2">Desa</h1>
+              </div>
+              <div className="col-span-3">
+                <Select
+                  name="desa"
+                  onValueChange={(e) => {
+                    handleChange("desa", e);
+                    setInput({ ...input, desa: e });
+                  }}
+                >
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Pilih" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {jenisWpOption.map((item, key) => {
+                      return (
+                        <SelectItem key={key} value={String(item.kode)}>
+                          {item.jenis_wp}
+                        </SelectItem>
+                      );
+                    })}
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-4 my-2">
+              <div className="col-span-1">
+                <h1 className="text-wrap">Kode Wilayah / Kode Pos</h1>
+              </div>
+              <div className="xl:col-span-1 col-span-3 flex">
+                <Input
+                  name="kode_area"
+                  onChange={(e) => {
+                    handleChange("kode_area", e.target.value);
+                    setInput({ ...input, kode_area: e.target.value });
+                  }}
+                  disabled={true}
+                  placeholder="automatically"
+                />
+                <h1 className="px-4 text-[20px] text-center">/</h1>
+                <Input
+                  name="kode_pos"
+                  title="Kode Pos"
+                  onChange={(e) => {
+                    handleChange("kode_pos", e.target.value);
+                    setInput({ ...input, kode_pos: e.target.value });
+                  }}
+                />
+              </div>
+            </div>
+
+            <InputHorizontal
               name="data_geometrik"
               title="Data Geometrik"
               onChange={(e) => {
@@ -280,20 +372,21 @@ export default function AlamatTabs() {
                 setInput({ ...input, data_geometrik: e.target.value });
               }}
             />
-          </div>
-          <div>
-            <h1>Disewa</h1>
-            <CheckSquare
-              name="disewa"
-              className="mt-2"
-              onChange={(e) => {
-                handleChange("disewa", e.target.value);
-                setInput({ ...input, disewa: e.target.value });
-              }}
-            />
-          </div>
-          <div>
-            <InputVertical
+
+            <div className="grid grid-cols-4 my-2 items-center">
+              <Label>Disewa</Label>
+              <Checkbox
+                name="disewa"
+                className="mt-2"
+                onChange={(e) => {
+                  handleChange("disewa", e.target.value);
+                  setInput({ ...input, disewa: e.target.value });
+                }}
+                checked={true}
+              />
+            </div>
+
+            <InputHorizontal
               name="tanggal_mulai"
               type="date"
               title="Tanggal Mulai"
@@ -302,20 +395,19 @@ export default function AlamatTabs() {
                 setInput({ ...input, tanggal_mulai: e.target.value });
               }}
             />
-          </div>
-          <div>
-            <InputVertical
+
+            <InputHorizontal
               type="date"
               name="tanggal_berakhir"
               title="Tanggal Berakhir"
+              className="my-2"
               onChange={(e) => {
                 handleChange("tanggal_berakhir", e.target.value);
                 setInput({ ...input, tanggal_berakhir: e.target.value });
               }}
             />
-          </div>
-          <div>
-            <InputVertical
+
+            <InputHorizontal
               type="text"
               name="kode_kpp"
               title="Kode Kantor Pelayanan Pajak"
@@ -324,26 +416,36 @@ export default function AlamatTabs() {
                 setInput({ ...input, kode_kpp: e.target.value });
               }}
             />
-          </div>
-          <div>
-            <h1>Kode Kantor Pelayanan Pajak</h1>
-            <Select
-              value={"" || undefined}
-              name="bagian_pengawasan"
-              onValueChange={(e) => {
-                handleChange("bagian_pengawasan", e);
-                setInput({ ...input, bagian_pengawasan: e });
-              }}
-            >
-              <SelectTrigger>
-                <SelectValue placeholder="Pilih" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem key={1} value={"kode"}>
-                  {"010"}
-                </SelectItem>
-              </SelectContent>
-            </Select>
+
+            <div className="grid grid-cols-4 my-2">
+              <div className="col-span-1">
+                <Label>Kode Kantor Pelayanan Pajak</Label>
+              </div>
+              <div className="col-span-3">
+                <Select
+                  value={"" || undefined}
+                  name="bagian_pengawasan"
+                  onValueChange={(e) => {
+                    handleChange("bagian_pengawasan", e);
+                    setInput({ ...input, bagian_pengawasan: e });
+                  }}
+                >
+                  <SelectTrigger>
+                    <SelectValue placeholder="Pilih" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem key={1} value={"kode"}>
+                      {"010"}
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+            <div className="col-span-2 float-end mt-10">
+              <Button pending={isPending} className="px-10 rounded-full">
+                Submit
+              </Button>
+            </div>
           </div>
         </div>
       </form>
