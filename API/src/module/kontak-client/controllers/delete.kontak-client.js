a31071fa5499ca1a6743601ad068b4232eea0d7b
irfangeliga <muhammad.2960@students.amikom.ac.id>
2025-08-25 08:44:36 +0700

    update APP&API module/kontak-client

--- API/src/module/kontak-client/controllers/delete.kontak-client.js
+++ API/src/module/kontak-client/controllers/delete.kontak-client.js
@@ -1,1 +1,17 @@
-export default async function deleteKontakClient(req) {}
+import { badRequest } from "../../../app/response.js";
+import MongodbWrapper from "../../../database/mongo/mongo.wrapper.js";
+import { masterClientSchema } from "../../master-client/master-client.schema.js";
+
+export default async function deleteKontakClient(req) {
+  const id = req.params.id;
+  const clientId = req.headers.clientId;
+
+  const wrapper = new MongodbWrapper(masterClientSchema());
+
+  const getData = await wrapper.getByFilter({ _id: clientId });
+  if (!getData.success) {
+    return badRequest({ message: getData.message });
+  }
+
+  const arrKontak = getData.data[0].data_kontak;
+}
